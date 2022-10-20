1a53760ec1bc0ff7e333784affd125b8e3341eb1
Kochuev.Dmitry <kochuevdmitry@googlemail.com>
2022-10-20 18:42:05 +0300

    Initial commit 2

--- src/main/java/com/example/ZeroneMailingMicroservice/service/KafkaListenerService.java
+++ src/main/java/com/example/ZeroneMailingMicroservice/service/KafkaListenerService.java
@@ -1,17 +1,27 @@
 package com.example.ZeroneMailingMicroservice.service;
 
 import com.example.ZeroneMailingMicroservice.dto.kafka.KafkaZeroneMailingDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaListenerService {
+
+    private final ZeroneMailSenderService zeroneMailSenderService;
 
     @KafkaListener(topics = "zeroneMailingTopic-1")
     void listener(KafkaZeroneMailingDto data) {
         log.info(data.toString());
+        try {
+            zeroneMailSenderService.emailSend(data.getEmail(), data.getTopic(), data.getBody());
+        } catch (Exception e){
+            log.info("Ошибка отправки почти, нужно вернуть сообщение в очередь кафки назад.");
+        }
+
     }
 
 }
